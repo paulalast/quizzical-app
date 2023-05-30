a3ab078dd66765086c99a92b4549856548f5e2a1
Paulina <paulalastowiecka@gmail.com>
2023-05-30 16:54:05 +0200

    n2

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react"
+import "@/styles/globals.css"
 import QuestionBox from "./QuestionBox"
 import { v4 as uuidv4 } from "uuid"
 import Confetti from "react-confetti"
